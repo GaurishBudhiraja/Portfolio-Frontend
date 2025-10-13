c332b3a5d3b297fd4742abd7d8f99c5bbe3140a1
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2025-10-13 18:02:48 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -42,6 +42,7 @@ import coverhunt from './company/coverhunt.png';
 import dcc from './company/dcc.png';
 import kelhel from './company/kelhel.png';
 import microverse from './company/microverse.png';
+import microsoftClub from './company/microsoft-club.png';
 
 import komikult from './projects/komikult.png';
 import leaderboard from './projects/leaderboard.png';
@@ -96,4 +97,6 @@ export {
   dcc,
   kelhel,
   microverse,
+  microsoftClub,
 };
+
